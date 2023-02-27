a6b167fb528f0d650a24b37b12b60de6dbebb11e
Iurii Demidov <demidov-iurii@yandex-team.ru>
2023-02-28 01:10:27 +0300

    Create YAC

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useEffect, useMemo, useState} from "react";
+import {INITIAL_STEP, INITIAL_VALUE, MAX_VALUE, VALUE_FIX_DELAY} from "./consts";
+import "./App.css"
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState(INITIAL_VALUE);
+  const [step, setStep] = useState(INITIAL_STEP);
+
+  const decrease = useCallback(() => {
+    setCount(prev => prev - step);
+  }, [step]);
+
+  const increase = useCallback(() => {
+    setCount(prev => prev + step);
+  }, [step]);
+
+  const changeStep = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const newStep = Number(event.target.value);
+
+    setStep(newStep < INITIAL_STEP ? INITIAL_STEP : newStep);
+  }, []);
+
+  useEffect(() => {
+    if (count > MAX_VALUE) {
+      setTimeout(() => {
+        setCount(INITIAL_VALUE);
+        alert(`Ha-ha-ha it will never be more than ${MAX_VALUE}!!!!!`);
+      }, VALUE_FIX_DELAY)
+    }
+  }, [count]);
+
+  const possibleValuesTillMax = useMemo(() => {
+    if (MAX_VALUE < count) {
+      return [];
+    }
+
+    return new Array(MAX_VALUE - count).fill(null).map((_, i) => count + 1 + i);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main className="app">
+        <h1 className="logo">
+          <span className="nonHoverableLogoPart"/>
+          <div className="logoPart">YET</div>
+          <div className="logoPart">ANOTHER</div>
+          <div className="logoPart">COUNTER</div>
+        </h1>
+        <div className="content">
+          <h2 className="contentTitle">
+            Current value is:&nbsp;
+            <span className={count > MAX_VALUE ? "wrongValue" : undefined}>{count}</span>
+          </h2>
+          <div className="actions">
+            <button className="actionButton" onClick={decrease}>Decrease</button>
+            <button className="actionButton" onClick={increase}>Increase</button>
+            <label>
+              Step&nbsp;
+              <input
+                  className="stepInput"
+                  value={step}
+                  placeholder="Step"
+                  type="number"
+                  onChange={changeStep}
+              />
+            </label>
+          </div>
+          <div>
+            Do you think that you can reach values more than {MAX_VALUE}??? No! Ha-ha-ha!!!<br/>
+            All values that you can enter are {possibleValuesTillMax.join(", ")}
+          </div>
+        </div>
+      </main>
   );
-}
+};
 
 export default App;
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const INITIAL_VALUE = 0;
+
+export const INITIAL_STEP = 1;
+
+export const MAX_VALUE = 10;
+
+export const VALUE_FIX_DELAY = 300; // ms
